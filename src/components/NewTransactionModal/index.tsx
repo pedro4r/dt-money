7fc71c1f79f1bd8f89d5bfd57686377a60e9cb92
Pedro Requiao <alonsofts@gmail.com>
2023-06-01 18:08:12 -0400

    Adding query feature to search form

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,8 +1,37 @@
 import * as Dialog from '@radix-ui/react-dialog';
 import { CloseButton, Content, Overlay, TransactionType, TransactionTypeButton } from './styles';
 import { ArrowCircleDown, ArrowCircleUp, X } from 'phosphor-react';
+import { Controller, useForm } from "react-hook-form";
+import * as z from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
+
+const newTransactionFormSchema = z.object({
+    description: z.string(),
+    price: z.number(),
+    category: z.string(),
+    type: z.enum(['income', 'outcome'])
+})
+
+type NewTransactionFormInputs = z.infer<typeof newTransactionFormSchema>
 
 export function NewTransactionModal() {
+
+    const { control,
+        register,
+        handleSubmit,
+        formState: { isSubmitting }
+    } = useForm<NewTransactionFormInputs>({
+        resolver: zodResolver(newTransactionFormSchema),
+        defaultValues: {
+            type: 'income'
+        }
+    })
+
+    async function handleCreateNewTransaction(data: NewTransactionFormInputs) {
+        await new Promise(resolve => setTimeout(resolve, 2000))
+        console.log(data)
+    }
+
     return (
         <Dialog.Portal>
             <Overlay>
@@ -11,21 +40,47 @@ export function NewTransactionModal() {
                     <CloseButton>
                         <X size={24} />
                     </CloseButton>
-                    <form action="">
-                        <input type="text" placeholder="Description" required />
-                        <input type="number" placeholder="Value" required />
-                        <input type="text" placeholder="Category" required />
+                    <form action="" onSubmit={handleSubmit(handleCreateNewTransaction)}>
+                        <input
+                            type="text"
+                            placeholder="Description"
+                            required
+                            {...register('description')}
+                        />
+                        <input
+                            type="number"
+                            placeholder="Value"
+                            required
+                            {...register('price', { valueAsNumber: true })}
+                        />
+                        <input
+                            type="text"
+                            placeholder="Category"
+                            required
+                            {...register('category')}
+                        />
 
-                        <TransactionType>
-                            <TransactionTypeButton variant="income" value="income">
-                                <ArrowCircleUp size={24} /> Inputs
-                            </TransactionTypeButton>
-                            <TransactionTypeButton variant="outcome" value="outcome">
-                                <ArrowCircleDown size={24} /> Output
-                            </TransactionTypeButton>
-                        </TransactionType>
+                        <Controller
+                            control={control}
+                            name="type"
+                            render={({ field }) => {
+                                return (
+                                    <TransactionType onValueChange={field.onChange} value={field.value}>
+                                        <TransactionTypeButton variant="income" value="income">
+                                            <ArrowCircleUp size={24} />
+                                            Inputs
+                                        </TransactionTypeButton>
 
-                        <button type="submit">Create</button>
+                                        <TransactionTypeButton variant="outcome" value="outcome">
+                                            <ArrowCircleDown size={24} />
+                                            Output
+                                        </TransactionTypeButton>
+                                    </TransactionType>
+                                )
+                            }}
+                        />
+
+                        <button type="submit" disabled={isSubmitting}>Create</button>
                     </form>
 
                 </Content>
--- src/components/NewTransactionModal/styles.ts
+++ src/components/NewTransactionModal/styles.ts
@@ -51,7 +51,12 @@ export const Content = styled(Dialog.Overlay)`
 
             cursor: pointer;
 
-            &:hover {
+            &:disabled {
+                opacity: 0.6;
+                cursor: not-allowed;
+            }
+
+            &:not(:disabled):hover {
                 background: ${props => props.theme['green-700']};
                 transition: background 0.2s;
             }
