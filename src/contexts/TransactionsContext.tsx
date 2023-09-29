31cb35eabf2cc8c61bdbf2af6edd153233afb5e6
Pedro Requiao <alonsofts@gmail.com>
2023-09-29 00:14:33 -0400

    feat: storage information at localstorage instead of json server - just to be able to use the application in production

--- src/contexts/TransactionsContext.tsx
+++ src/contexts/TransactionsContext.tsx
@@ -1,6 +1,7 @@
 import { ReactNode, useEffect, useState, useCallback } from "react";
-import { api } from "../lid/axios";
+// import { api } from "../lid/axios";
 import { createContext } from "use-context-selector";
+import { v4 as uuidv4 } from "uuid";
 
 interface Transaction {
     id: number,
@@ -33,34 +34,65 @@ export const TransactionsContext = createContext({} as TransactionContextType);
 
 export function TransactionProvider({ children }: TransactionProviderProps) {
 
-
     const [transactions, setTransactions] = useState<Transaction[]>([]);
 
     const fetchTransactions = useCallback(async (query?: string) => {
 
-        const response = await api.get('transactions', {
-            params: {
-                _sort: 'createdAt',
-                _order: 'desc',
-                q: query,
-            }
-        });
 
-        setTransactions(response.data);
+        const storedStateAsJSON = localStorage.getItem('@dt-money:transactions-1.0.0') || '[]';
+        const storedStateAsArray = JSON.parse(storedStateAsJSON);
+
+        // storedStateAsArray.push(data);
+        // const updatedStateAsJSON = JSON.stringify(storedStateAsArray);
+        // localStorage.setItem('@dt-money:transactions-1.0.0', updatedStateAsJSON);
+
+        // const response = await api.get('transactions', {
+        //     params: {
+        //         _sort: 'createdAt',
+        //         _order: 'desc',
+        //         q: query,
+        //     }
+        // });
+
+        setTransactions(storedStateAsArray);
     }, [])
 
     const createTransaction = useCallback(async (data: CreateTransactionInput) => {
+
         const { description, price, category, type } = data;
 
-        const response = await api.post('transactions', {
+        const transactionData = {
+            id: uuidv4(),
             description,
             category,
             price,
             type,
             createdAt: new Date(),
-        })
+        }
 
-        setTransactions(state => [response.data, ...state])
+        const storedStateAsJSON = localStorage.getItem('@dt-money:transactions-1.0.0') || '[]';
+        const storedStateAsArray = JSON.parse(storedStateAsJSON);
+
+        storedStateAsArray.push(transactionData);
+        const updatedStateAsJSON = JSON.stringify(storedStateAsArray);
+        localStorage.setItem('@dt-money:transactions-1.0.0', updatedStateAsJSON);
+
+        setTransactions(storedStateAsArray)
+
+        //JSON SERVER BELLOW
+
+        // const { description, price, category, type } = data;
+
+        // const response = await api.post('transactions', {
+        //     description,
+        //     category,
+        //     price,
+        //     type,
+        //     createdAt: new Date(),
+        // })
+
+        // setTransactions(state => [response.data, ...state])
+        
     }, [])
 
     useEffect(() => {
