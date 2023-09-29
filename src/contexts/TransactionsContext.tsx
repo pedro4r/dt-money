950b29606ed6e2972d1e10595bb6ccec7ba844e4
Pedro Requiao <alonsofts@gmail.com>
2023-09-29 00:25:45 -0400

    fix: Transaction query error

--- src/contexts/TransactionsContext.tsx
+++ src/contexts/TransactionsContext.tsx
@@ -1,5 +1,4 @@
 import { ReactNode, useEffect, useState, useCallback } from "react";
-// import { api } from "../lid/axios";
 import { createContext } from "use-context-selector";
 import { v4 as uuidv4 } from "uuid";
 
@@ -14,7 +13,7 @@ interface Transaction {
 
 interface TransactionContextType {
     transactions: Transaction[]
-    fetchTransactions: (query?: string) => Promise<void>;
+    fetchTransactions: () => void;
     createTransaction: (data: CreateTransactionInput) => Promise<void>
 }
 
@@ -29,31 +28,15 @@ interface CreateTransactionInput {
     type: 'income' | 'outcome';
 }
 
-
 export const TransactionsContext = createContext({} as TransactionContextType);
 
 export function TransactionProvider({ children }: TransactionProviderProps) {
 
     const [transactions, setTransactions] = useState<Transaction[]>([]);
 
-    const fetchTransactions = useCallback(async (query?: string) => {
-
-
+    const fetchTransactions = useCallback(() => {
         const storedStateAsJSON = localStorage.getItem('@dt-money:transactions-1.0.0') || '[]';
         const storedStateAsArray = JSON.parse(storedStateAsJSON);
-
-        // storedStateAsArray.push(data);
-        // const updatedStateAsJSON = JSON.stringify(storedStateAsArray);
-        // localStorage.setItem('@dt-money:transactions-1.0.0', updatedStateAsJSON);
-
-        // const response = await api.get('transactions', {
-        //     params: {
-        //         _sort: 'createdAt',
-        //         _order: 'desc',
-        //         q: query,
-        //     }
-        // });
-
         setTransactions(storedStateAsArray);
     }, [])
 
@@ -78,20 +61,6 @@ export function TransactionProvider({ children }: TransactionProviderProps) {
         localStorage.setItem('@dt-money:transactions-1.0.0', updatedStateAsJSON);
 
         setTransactions(storedStateAsArray)
-
-        //JSON SERVER BELLOW
-
-        // const { description, price, category, type } = data;
-
-        // const response = await api.post('transactions', {
-        //     description,
-        //     category,
-        //     price,
-        //     type,
-        //     createdAt: new Date(),
-        // })
-
-        // setTransactions(state => [response.data, ...state])
         
     }, [])
 
